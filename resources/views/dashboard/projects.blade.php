f2bc1e759ea63f2231c96244f6bf1b190839e790
nachobonilla6 <nachobonilla6@gmail.com>
2026-01-25 01:34:50 -0600

    style: change projects grid to 3 columns per row

--- resources/views/dashboard/projects.blade.php
+++ resources/views/dashboard/projects.blade.php
@@ -126,7 +126,7 @@
                 </button>
             </div>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 @foreach($projects as $project)
                 @php
                     $imgs = is_array($project->images) ? $project->images : json_decode($project->images, true);
